3e39eb0d339f76a35edfe1d4a9c449a8ee109e1f
Zoltan Kochan <z@kochan.io>
2018-01-13 21:22:11 +0200

    fix: use @pnpm/self-installer version 2

--- src/installPnpm.ts
+++ src/installPnpm.ts
@@ -3,26 +3,21 @@ import fs = require('fs')
 import got = require('got')
 import path = require('path')
 
+const installFile = require.resolve('@pnpm/self-installer')
+
 export default async () => {
-  const installFile = path.join(__dirname, 'install.js')
-
   await new Promise((resolve, reject) => {
-    got.stream('https://unpkg.com/@pnpm/self-installer')
-      .pipe(fs.createWriteStream(installFile))
-      .on('error', reject)
-      .on('close', () => {
-        const proc = spawn('node', [installFile], {
-          stdio: 'inherit',
-        })
+    const proc = spawn('node', [installFile], {
+      stdio: 'inherit',
+    })
 
-        proc.on('error', reject)
+    proc.on('error', reject)
 
-        proc.on('close', (code: number) => {
-          if (code > 0) {
-            return reject(new Error(`Exit code: ${code}`))
-          }
-          return resolve()
-        })
-      })
+    proc.on('close', (code: number) => {
+      if (code > 0) {
+        return reject(new Error(`Exit code: ${code}`))
+      }
+      return resolve()
+    })
   })
 }
